eab421ef05f0bdbdf6993dbcf1a2fd8f8eb25f05
Palash <mobiledev.palash@gmail.com>
2021-11-02 21:40:46 +0530

    Initial commit -spring-learning-project

---
+++ src/main/java/com/palkos/springlearningproject/SpringLearningProjectApplication.java
@@ -0,0 +1,13 @@
+package com.palkos.springlearningproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palkos/springlearningproject/SpringLearningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palkos.springlearningproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
